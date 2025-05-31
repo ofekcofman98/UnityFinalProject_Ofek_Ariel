e94c9309add516cb7f788b3aa8aa99fde838bead
ofek <ofekcofman98@gmail.com>
2025-05-31 18:27:25 +0300

    added interaction missions, adjusted the editor

--- Assets/Scripts/Interact/InteractableObject.cs
+++ Assets/Scripts/Interact/InteractableObject.cs
@@ -15,10 +15,15 @@ public class InteractableObject : MonoBehaviour
 {
     public string objectName;
     public eInteractableType interactableType;
+    [SerializeField] private string interactableId;
+    public string InteractableId => interactableId;
+
 
     public virtual void Interact()
     {
         Debug.Log($"Interacted with {objectName}");
+
+        MissionsManager.Instance.ValidateInteractableMission(interactableId);
     }
 
 }
---
+++ Assets/Scripts/Missions/InteractableMissionData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SQL Mission", menuName = "SQL Detective/Mission/Interactable Mission")]
+public class InteractableMissionData : MissionData
+{
+    public string requiredObjectId;
+    private string _lastTriggeredId;
+
+    public void SetTriggeredObject(string id)
+    {
+        _lastTriggeredId = id;
+    }
+
+    public override bool Validate()
+    {
+        return _lastTriggeredId == requiredObjectId;
+    }
+
+}
