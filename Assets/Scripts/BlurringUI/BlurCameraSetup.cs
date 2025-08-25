d2318fd2b94426c2d5dfeec0e23934e8bb2faa65
ofek <ofekcofman98@gmail.com>
2025-08-25 17:16:31 +0300

    without urp

--- Assets/Scripts/BlurringUI/BlurCameraSetup.cs
+++ Assets/Scripts/BlurringUI/BlurCameraSetup.cs
@@ -1,56 +1,56 @@
-// BlurCameraSetup.cs
-// Attach this to an empty GameObject in your scene to auto-create and configure the Blur Camera
-using UnityEngine;
+// // BlurCameraSetup.cs
+// // Attach this to an empty GameObject in your scene to auto-create and configure the Blur Camera
+// using UnityEngine;
 
-[ExecuteInEditMode]
-public class BlurCameraSetup : MonoBehaviour
-{
-    public Camera mainCamera;
-    public RenderTexture blurTexture;
-    public Material blurMaterial;
+// [ExecuteInEditMode]
+// public class BlurCameraSetup : MonoBehaviour
+// {
+//     public Camera mainCamera;
+//     public RenderTexture blurTexture;
+//     public Material blurMaterial;
 
-    private Camera blurCamera;
+//     private Camera blurCamera;
 
-    void Start()
-    {
-        if (!mainCamera) mainCamera = Camera.main;
-        if (!blurTexture)
-        {
-            blurTexture = new RenderTexture(Screen.width / 2, Screen.height / 2, 16);
-            blurTexture.name = "BlurredScene";
-        }
+//     void Start()
+//     {
+//         if (!mainCamera) mainCamera = Camera.main;
+//         if (!blurTexture)
+//         {
+//             blurTexture = new RenderTexture(Screen.width / 2, Screen.height / 2, 16);
+//             blurTexture.name = "BlurredScene";
+//         }
 
-        SetupBlurCamera();
-        SetupBlurMaterial();
-    }
+//         SetupBlurCamera();
+//         SetupBlurMaterial();
+//     }
 
-    void SetupBlurCamera()
-    {
-        if (blurCamera == null)
-        {
-            GameObject camObj = new GameObject("BlurCamera");
-            blurCamera = camObj.AddComponent<Camera>();
-        }
+//     void SetupBlurCamera()
+//     {
+//         if (blurCamera == null)
+//         {
+//             GameObject camObj = new GameObject("BlurCamera");
+//             blurCamera = camObj.AddComponent<Camera>();
+//         }
 
-        blurCamera.CopyFrom(mainCamera);
-        blurCamera.transform.SetPositionAndRotation(mainCamera.transform.position, mainCamera.transform.rotation);
+//         blurCamera.CopyFrom(mainCamera);
+//         blurCamera.transform.SetPositionAndRotation(mainCamera.transform.position, mainCamera.transform.rotation);
 
-        blurCamera.targetTexture = blurTexture;
-        blurCamera.depth = mainCamera.depth - 1;
-        blurCamera.clearFlags = CameraClearFlags.SolidColor;
-        blurCamera.backgroundColor = new Color(0, 0, 0, 0);
+//         blurCamera.targetTexture = blurTexture;
+//         blurCamera.depth = mainCamera.depth - 1;
+//         blurCamera.clearFlags = CameraClearFlags.SolidColor;
+//         blurCamera.backgroundColor = new Color(0, 0, 0, 0);
 
-        // Exclude UI and popups
-        blurCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("UI"));
+//         // Exclude UI and popups
+//         blurCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("UI"));
 
-        blurCamera.gameObject.hideFlags = HideFlags.DontSave;
-    }
+//         blurCamera.gameObject.hideFlags = HideFlags.DontSave;
+//     }
 
-    void SetupBlurMaterial()
-    {
-        if (blurMaterial)
-        {
-            blurMaterial.SetFloat("_BlurSize", 1.5f); // tweak blur here
-        }
-    }
-}
+//     void SetupBlurMaterial()
+//     {
+//         if (blurMaterial)
+//         {
+//             blurMaterial.SetFloat("_BlurSize", 1.5f); // tweak blur here
+//         }
+//     }
+// }
--- Assets/Scripts/ScreenSaver/ScreensaverController.cs
+++ Assets/Scripts/ScreenSaver/ScreensaverController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.UI;
 
 public class ScreensaverController
 {
